5f77291ca4232614ab4bdfda0178cb824ece977c
Song Seung-hyun <thdtmdgus5505@gmail.com>
2021-12-13 19:45:34 +0900

    config: initial commit
    initial commit

---
+++ src/main/java/com/songstone/coronaline/CoronaLineApplication.java
@@ -0,0 +1,13 @@
+package com.songstone.coronaline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaLineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoronaLineApplication.class, args);
+    }
+
+}
