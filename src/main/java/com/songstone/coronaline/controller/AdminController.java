89c8a21975a4e71c20e27710579698e6a1baaf8b
Song Seung-hyun <thdtmdgus5505@gmail.com>
2021-12-17 15:56:12 +0900

    feat: implements adminPlaces
    adminPlaces 메서드 구현

--- src/main/java/com/songstone/coronaline/controller/AdminController.java
+++ src/main/java/com/songstone/coronaline/controller/AdminController.java
@@ -1,17 +1,32 @@
 package com.songstone.coronaline.controller;
 
+import com.songstone.coronaline.constant.PlaceType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RequestMapping("/admin")
 @Controller
 public class AdminController {
 
     @GetMapping("/places")
-    public String adminPlaces() {
-        return "admin/places";
+    public ModelAndView adminPlaces( //RequestParam을 넣어주면 있어야만 하고 생략하면 있어도되고 없어도 된다
+            PlaceType placeType,
+            String placeName,
+            String address
+    ) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("placeType", placeType);
+        map.put("placeName", placeName);
+        map.put("address", address);
+
+        return new ModelAndView("admin/places", map);
     }
 
     @GetMapping("/places/{placeId}")
