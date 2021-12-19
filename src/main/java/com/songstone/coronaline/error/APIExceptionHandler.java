8786b3d36407aa7e5a563e7eb7d27dfdc4d5dd45
Song Seung-hyun <thdtmdgus5505@gmail.com>
2021-12-19 23:24:49 +0900

    add: implements Exception Handler
    implements Exception Handler

---
+++ src/main/java/com/songstone/coronaline/error/APIExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.songstone.coronaline.error;
+
+import com.songstone.coronaline.Exception.GeneralException;
+import com.songstone.coronaline.constant.ErrorCode;
+import com.songstone.coronaline.dto.APIErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+@RestControllerAdvice(annotations = RestController.class) // 레스트 컨트롤러의 동작을 감시
+public class APIExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<APIErrorResponse> general(GeneralException e){
+        ErrorCode errorCode = e.getErrorCode();
+        HttpStatus status = errorCode.isClientSideError() ?
+                HttpStatus.BAD_REQUEST :
+                HttpStatus.INTERNAL_SERVER_ERROR;
+
+        return ResponseEntity
+                .status(status)
+                .body(APIErrorResponse.of(
+                        false, errorCode, errorCode.getMessage(e)
+                ));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<APIErrorResponse> exception(Exception e){
+        ErrorCode errorCode = ErrorCode.INTERNAL_ERROR;
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        return ResponseEntity
+                .status(status)
+                .body(APIErrorResponse.of(
+                        false, errorCode, errorCode.getMessage(e)
+                ));
+    }
+}
