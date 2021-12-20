97a7bb7e1feb5c75e4031c7cd4756c79e700adb7
Song Seung-hyun <thdtmdgus5505@gmail.com>
2021-12-20 14:39:42 +0900

    refactor: refactor BaseControllerTest
    @WebMvcTest

--- src/test/java/com/songstone/coronaline/controller/BaseControllerTest.java
+++ src/test/java/com/songstone/coronaline/controller/BaseControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,8 +15,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@AutoConfigureMockMvc
-@SpringBootTest
+@WebMvcTest(BaseController.class)
 class BaseControllerTest {
 
     @Autowired
